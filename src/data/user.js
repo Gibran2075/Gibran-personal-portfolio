13472cfcb299dd2fb5e990b1c10500f292c2495c
Gibran <m.giibraan@gmail.com>
2025-10-13 08:43:30 +0700

    add data user

--- src/data/user.js
+++ src/data/user.js
@@ -1,83 +1,86 @@
 const INFO = {
-	main: {
-		title: "Reactfolio by Muhammad Gibran",
-		name: "Muhammad Gibran",
-		email: "mhd.giibraan@gmail.com",
-		logo: "../Gibran_ico.png",
-	},
+  main: {
+    title: "Reactfolio by Muhammad Gibran",
+    name: "Muhammad Gibran",
+    email: "mhd.giibraan@gmail.com",
+    logo: "../Gibran_ico.png",
+  },
 
-	socials: {
-		github: "https://github.com/Gibran2075?tab=repositories",
-		linkedin: "https://www.linkedin.com/in/mhd-gibran?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-		instagram: "https://www.instagram.com/mhd_gbrn?igsh=MXFpbTFmNzQwcmNjaw==",
-	},
+  socials: {
+    github: "https://github.com/Gibran2075?tab=repositories",
+    linkedin:
+      "https://www.linkedin.com/in/mhd-gibran?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    instagram: "https://www.instagram.com/mhd_gbrn?igsh=MXFpbTFmNzQwcmNjaw==",
+  },
 
-	homepage: {
-		title: "Hi, I'm Gibran — Frontend Web Developer.",
-		description:
-			"I am a Fullstack Web Developer with 2+ years of hands-on experience building modern, scalable, and secure applications. My expertise includes HTML, CSS, JavaScript, and Python, along with frameworks such as React.js, Vue.js, Tailwind CSS, and Flask. \
+  homepage: {
+    title: "Hi, I'm Gibran — Frontend Web Developer.",
+    description:
+      "I am a Fullstack Web Developer with 2+ years of hands-on experience building modern, scalable, and secure applications. My expertise includes HTML, CSS, JavaScript, and Python, along with frameworks such as React.js, Vue.js, Tailwind CSS, and Flask. \
 			I specialize in creating responsive, user-friendly digital solutions that deliver real business value. Passionate about problem-solving and clean, maintainable code, I continuously explore new technologies to stay ahead of industry trends. \
 			If you’re looking for a reliable freelance developer to bring your ideas to life and deliver high-quality results, I’m ready to collaborate.",
-	},
+  },
 
-	about: {
-		title: "I’m Muhammad Gibran. I live in Indonesia, where I design the future.",
-		description:
-			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
-	},
+  about: {
+    title:
+      "I’m Muhammad Gibran. I live in Indonesia, where I design the future.",
+    description:
+      "I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+  },
 
-	articles: {
-		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
-		description:
-			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
-	},
+  articles: {
+    title:
+      "I'm passionate about the boundaries of what's possible and inspiring the next generation of innovators.",
+    description:
+      "Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+  },
 
-	projects: [
-		{
-			title: "Real Estate | Landing Page",
-			description:
-				"A modern and elegant real estate landing page featuring sleek design, smooth animations, and a fully responsive user experience that captures attention and drives engagement.",
-			logo: "../react_js.png",
-			linkText: "View Project",
-			link: "https://real-estate-portfolio-azure.vercel.app/",
-		},
+  projects: [
+    {
+      title: "Real Estate | Landing Page",
+      description:
+        "A modern and elegant real estate landing page featuring sleek design, smooth animations, and a fully responsive user experience that captures attention and drives engagement.",
+      logo: "../react_js.png",
+      linkText: "View Project",
+      link: "https://real-estate-portfolio-azure.vercel.app/",
+    },
 
-		// {
-		// 	title: "Web Film",
-		// 	description:
-		// 		"MovieVerse – Eksplorasi Dunia Film dengan React JS, Temukan Rekomendasi & Detail Favoritmu",
-		// 	logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
-		// 	linkText: "View Project",
-		// 	link: "https://github.com",
-		// },
+    // {
+    // 	title: "Web Film",
+    // 	description:
+    // 		"MovieVerse – Eksplorasi Dunia Film dengan React JS, Temukan Rekomendasi & Detail Favoritmu",
+    // 	logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+    // 	linkText: "View Project",
+    // 	link: "https://github.com",
+    // },
 
-		// {
-		// 	title: "Web API Dashboard",
-		// 	description:
-		// 		"Web Dashboard dibuat dengan ReactJs dan ChartJs untuk menampilkan data dari team data engineer agar menampilkan data yang dinamis dan interaktif.",
-		// 	logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
-		// 	linkText: "View Project",
-		// 	link: "https://github.com",
-		// },
+    // {
+    // 	title: "Web API Dashboard",
+    // 	description:
+    // 		"Web Dashboard dibuat dengan ReactJs dan ChartJs untuk menampilkan data dari team data engineer agar menampilkan data yang dinamis dan interaktif.",
+    // 	logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+    // 	linkText: "View Project",
+    // 	link: "https://github.com",
+    // },
 
-		{
-			title: "Template Web Car Rental Service",
-			description:
-				"A modern and responsive car rental service UI built with React, Tailwind CSS, and Shadcn, featuring a clean layout and smooth user interface design.",
-			logo: "../react_js.png",
-			linkText: "View Project",
-			link: "https://car-rental-service-sepia.vercel.app/",
-		},
+    {
+      title: "Template Web Car Rental Service",
+      description:
+        "A modern and responsive car rental service UI built with React, Tailwind CSS, and Shadcn, featuring a clean layout and smooth user interface design.",
+      logo: "../react_js.png",
+      linkText: "View Project",
+      link: "https://car-rental-service-sepia.vercel.app/",
+    },
 
-		{
-			title: "Node JS API for Web Application",
-			description:
-				"API backend ringan dan scalable berbasis Node.js untuk mendukung aplikasi mobile dengan performa cepat dan integrasi mulus.",
-			logo: "../node_js.png",
-			linkText: "View Project",
-			link: "https://github.com/Gibran2075/gym_exercise",
-		},
-	],
+    {
+      title: "Node JS API for Web Application",
+      description:
+        "API backend ringan dan scalable berbasis Node.js untuk mendukung aplikasi mobile dengan performa cepat dan integrasi mulus.",
+      logo: "../node_js.png",
+      linkText: "View Project",
+      link: "https://github.com/Gibran2075/gym_exercise",
+    },
+  ],
 };
 
 export default INFO;
