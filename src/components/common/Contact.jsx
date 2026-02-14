58207f080778f72a1f80f5161b15328b177c0beb
Gibran <m.giibraan@gmail.com>
2026-02-14 18:35:40 +0700

    Update contact

--- src/components/common/Contact.jsx
+++ src/components/common/Contact.jsx
@@ -33,7 +33,7 @@ const Contact = () => {
   return (
     <div
     className='text-center p-6 py-20 lg:px-32 w-full overflow-hidden' id='Contact'>
-        <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Contact <span className='underline underline-offset-4 decoration-1 under font-light'>With Us</span></h1>
+        <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Contact <span className='underline underline-offset-4 decoration-1 under font-light'>With Me</span></h1>
         <p className='text-center tex-gray-500 mb-12 max-w-80 mx-auto'>Ready to make a move? Let's Build Your Future Together</p>
 
 
