32722a71bec558eff37e781f762ecd3f726a1b90
Gibran2075 <m.giibraan@gmail.com>
2026-08-03 11:59:19 +0700

    fixing error image

--- src/data/user.js
+++ src/data/user.js
@@ -3,7 +3,7 @@ import Project2 from "../assets/Project2.png"
 import Project3 from "../assets/Project3.png"
 import Project4 from "../assets/Project4.png"
 import Project5 from "../assets/Project5.png"
-import Project6 from "../assets/Project6.png"
+import Project6 from "../assets/Project7.png"
 
 const INFO = {
   main: {
